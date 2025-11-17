9e6cd97d8e0d6c3f57835c8c9386d74789459d8a
Alba Bernal <alba.bernal15@gmail.com>
2025-11-17 18:16:01 +0100

    Identificar atributos

--- src/main/java/org/educa/entity/AlquilerEntity.java
+++ src/main/java/org/educa/entity/AlquilerEntity.java
@@ -1,18 +1,28 @@
 package org.educa.entity;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
-public class AlquilerEntity {
+@Entity
+@Table(name = "alquiler")
+public class AlquilerEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID=1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Integer id;
+
     private LocalDate fechaIni;
     private LocalDate fechaFin;
     private BigDecimal precio;
